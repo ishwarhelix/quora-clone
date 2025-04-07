b8d2742c529ec26b555ab26e7e8081eabde85ad8
unknown <ishwar@example.com>
2025-04-07 17:06:22 +0530

    first commit

---
+++ quora_clone/questions/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='liked_answers', blank=True)
+
+    def __str__(self):
+        return f'Answer by {self.author.username} on {self.question.title}'
---
+++ quora_clone/questions/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Question, Answer
+from .forms import QuestionForm, AnswerForm
+
+def home(request):
+    questions = Question.objects.all().order_by('-created_at')
+    return render(request, 'questions/home.html', {'questions': questions})
+
+@login_required
+def ask_question(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.save()
+            return redirect('question_detail', pk=question.pk)
+    else:
+        form = QuestionForm()
+    return render(request, 'questions/ask_question.html', {'form': form})
+
+def question_detail(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    answers = question.answers.all().order_by('-created_at')
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.question = question
+            answer.save()
+            return redirect('question_detail', pk=pk)
+    else:
+        form = AnswerForm()
+    return render(request, 'questions/question_detail.html', 
+                 {'question': question, 'answers': answers, 'form': form})
+
+@login_required
+def like_answer(request, answer_id):
+    answer = get_object_or_404(Answer, id=answer_id)
+    if request.user in answer.likes.all():
+        answer.likes.remove(request.user)
+    else:
+        answer.likes.add(request.user)
+    return redirect('question_detail', pk=answer.question.pk) 

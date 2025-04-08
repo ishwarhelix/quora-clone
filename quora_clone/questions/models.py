7551d1ae416f4c0df75bee9aa306e07811b56738
unknown <ishwar@example.com>
2025-04-08 14:38:24 +0530

    this is a working model with few imporvements

--- quora_clone/questions/models.py
+++ quora_clone/questions/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Question(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    topic = models.CharField(max_length=200)
+    question = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.question
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
@@ -20,4 +20,4 @@ class Answer(models.Model):
     likes = models.ManyToManyField(User, related_name='liked_answers', blank=True)
 
     def __str__(self):
-        return f'Answer by {self.author.username} on {self.question.title}'
+        return f'Answer by {self.author.username} on {self.question.question}'
